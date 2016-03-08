48a3e231f156f0a83f7ed555ccaaed196a5be1e3
CameronAnderson <cameron.anderson@students.olin.edu>
2016-03-07 19:53:57 -0500

    Modified blink code to show a green led when rope is attached and a red when rope has disconnected

--- blink/blink.c
+++ blink/blink.c
@@ -1,27 +1,38 @@
 #include <p24FJ128GB206.h>
+#include <stdio.h>
 #include "config.h"
 #include "common.h"
 #include "ui.h"
 #include "timer.h"
+#include "pin.h"
+#include "uart.h"
 
 int16_t main(void) {
     init_clock();
     init_ui();
     init_timer();
+    init_pin();
+    init_uart();
 
-    led_on(&led1);
-    timer_setPeriod(&timer2, 0.5);
-    timer_start(&timer2);
+    uint16_t rope = &D[0];
+    pin_digitalIn(rope);
+
 
     while (1) {
-        if (timer_flag(&timer2)) {
-            timer_lower(&timer2);
-            led_toggle(&led3);
-
+        uint16_t value = pin_read(rope);
+        if(value == 0){
+            led_on(&led1);
+            led_off(&led2);
+            printf("Low%s\n\r");
         }
-        led_write(&led2, !sw_read(&sw2));
-        led_write(&led1, !sw_read(&sw3));
+        if(value == 1){
+            led_on(&led2);
+            led_off(&led1);
+            printf("High%s\n\r");
+        }
+        }
+            
 
     }
-}
+
 
