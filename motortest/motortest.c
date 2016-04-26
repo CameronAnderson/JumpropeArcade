58123e03c43bae83ce2a0024c0064d7b32be31dd
SKaragianes <sean.karagianes@students.olin.edu>
2016-04-26 13:36:25 -0400

    Non-working motortest code for Brad to help us with

--- motortest/motortest.c
+++ motortest/motortest.c
@@ -7,53 +7,61 @@
 #include "timer.h"
 #include "pin.h"
 #include "oc.h"
-#include "uart.h"
+// #include "uart.h"
 
-_PIN *dirpin1, *pwmpin1, *potentiometer1;
-
-uint16_t actual_position1 = 0;
-
-43700
-20600
-33500
-
-
-int16_t main(void) {
-    init_clock();
-    init_ui();
-    init_timer();
-    init_pin();
-    init_oc();
-    init_uart();
-
-    // dirpin1 = &D[8];  //set direction control pin as pin 8
-    // pwmpin1 = &D[9];  //set PWM pin as pin 9 
-    potentiometer1 = &A[5];
-
-    //initialize the two pins as digital outputs and then clear them
-    // pin_digitalOut(dirpin1);
-    // pin_digitalOut(pwmpin1);
-    pin_analogIn(potentiometer1);    
-
-
-    // pin_clear(dirpin1);
-    // pin_clear(pwmpin1);
-
-    timer_setPeriod(&timer1, .1);
-    timer_start(&timer1);
-
-    // uint16_t duty1 = 0xffff;
-
-    while (1) {
-
-        if (timer_flag(&timer1)) {
-            timer_lower(&timer1);
-            actual_position1 = pin_read(potentiometer1);
-            printf("Pot%u\n\r", actual_position1);            
-        }
-
-
-        // oc_pwm(&oc1, pwmpin1, &timer5, 1e3, duty1/10);
-
-    }
-}
+_PIN *dirpin, *pwmpin;
+ 
+ int16_t main(void) {
+     init_clock();
+     init_ui();
+     init_timer();
+     init_pin();
+     init_oc();
+ 
+     dirpin = &D[8];  //set direction control pin as pin 8
+     pwmpin = &D[9];  //set PWM pin as pin 9 
+ 
+     //initialize the two pins as digital outputs and then clear them
+     pin_digitalOut(dirpin);
+     pin_digitalOut(pwmpin);
+ 
+     pin_clear(dirpin);
+     pin_clear(pwmpin);
+ 
+     timer_setPeriod(&timer1, 1);
+     timer_start(&timer1);
+ 
+     uint16_t duty = 0x8000;
+     float freq = 1000;
+ 
+     while (1) {
+ 
+         led_off(&led2);
+         led_off(&led3);
+         oc_pwm(&oc2, pwmpin, NULL, freq, 0);
+ 
+         while (!sw_read(&sw2)){
+             led_on(&led2);
+             pin_clear(dirpin);
+             oc_pwm(&oc2, pwmpin, NULL, freq, duty);
+         }
+ 
+         while (!sw_read(&sw3)){
+             led_on(&led3);
+             pin_set(dirpin);
+             oc_pwm(&oc2, pwmpin, NULL, freq, duty);
+         }
+ 
+         // oc_pwm(&oc1, &D[9], &timer5, freq, 0);
+         // pin_write(&D[9], duty);
+         
+         // if (timer_flag(&timer1)) {
+ 
+         //     timer_lower(&timer1);
+         //     led_toggle(&led1);
+ 
+         //     pin_toggle(dirpin);
+ 
+         // }
+     }
+ } 
